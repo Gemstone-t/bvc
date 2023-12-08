6c6c739a31d95fc605744d1f57681d02f334e13f
Gemstone-t <crystalDev1003@gmail.com>
2023-12-08 06:58:30 -0800

    revised-1

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,9 @@
-$('.dropdown').click(function () {
-    $(this).attr('tabindex', 1).focus();
-    $(this).toggleClass('active');
-    $(this).find('.dropdown-menu').slideToggle(300);
-});
-$('.dropdown').focusout(function () {
-    $(this).removeClass('active');
-    $(this).find('.dropdown-menu').slideUp(300);
-});
-$('.dropdown .dropdown-menu li').click(function () {
-    $(this).parents('.dropdown').find('span').text($(this).text());
-    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-});
-
-
+new Splide( '.splide' , {
+    type    : 'loop',
+    perPage : 4,
+    focus   : 'center',
+    perMove : 1,
+    gap     : '0',
+    autoplay: true,
+    interval: 2000,
+  }).mount();
