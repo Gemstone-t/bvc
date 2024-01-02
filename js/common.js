d4a4374230f5f9552911e3274e35940ea0ee9ae0
SmileDev10162 <brian.smile.10162@gmail.com>
2024-01-02 18:59:55 -0500

    font-size by vw

--- js/common.js
+++ js/common.js
@@ -24,46 +24,46 @@ function adjustStyles () {
   }
 
   var e_str = [
-    { name: 'h1', fontSize: 54, lineHeight: 60 },
-    { name: 'h2', fontSize: 46, lineHeight: 48 },
-    { name: 'h2.alt', fontSize: 46, lineHeight: 50 },
-    { name: 'h3', fontSize: 30, lineHeight: 32 },
-    { name: 'h3.alt', fontSize: 35, lineHeight: 35 },
-    { name: 'h4', fontSize: 23, lineHeight: 24 },
-    { name: 'h5', fontSize: 19, lineHeight: 21 },
-    { name: 'a', fontSize: 20, lineHeight: 27 },
-    { name: 'select', fontSize: 20, lineHeight: 20 },
-    { name: 'label', fontSize: 20, lineHeight: 20 },
-    { name: 'input', fontSize: 20, lineHeight: 20 },
-    { name: 'a.nav-link', fontSize: 20, lineHeight: 25 },
-    { name: 'a.utility-nav', fontSize: 15, lineHeight: 15 },
-    { name: 'p.eyebrow', fontSize: 22, lineHeight: 22 },
-    { name: 'button', fontSize: 20, lineHeight: 20 },
-    { name: '.button', fontSize: 20, lineHeight: 20 },
-    { name: 'p.XXL', fontSize: 50, lineHeight: 65 },
-    { name: 'p.XL', fontSize: 26, lineHeight: 32 },
-    { name: 'p.L', fontSize: 24, lineHeight: 35 },
-    { name: 'p.M', fontSize: 20, lineHeight: 27 },
-    { name: 'p.S', fontSize: 18, lineHeight: 27 },
-    { name: 'p.XS', fontSize: 16, lineHeight: 19 },
-    { name: 'p.call-out', fontSize: 39, lineHeight: 39 },
-    { name: 'a.text-link', fontSize: 20, lineHeight: 27 },
-    { name: 'body .footer label.copyright', fontSize: 14, lineHeight: 27 },
-    {
-      name: 'body .nav-tool-bar .nav-home .menu ul li a',
-      fontSize: 17,
-      lineHeight: 25
-    },
-    {
-      name: 'body .footer .footer-right .link-btn a',
-      fontSize: 15,
-      lineHeight: 15
-    },
-    {
-      name: 'body .footer .footer-right .link-group .link-list a',
-      fontSize: 17,
-      lineHeight: 27
-    }
+    // { name: 'h1', fontSize: 54, lineHeight: 60 },
+    // { name: 'h2', fontSize: 46, lineHeight: 48 },
+    // { name: 'h2.alt', fontSize: 46, lineHeight: 50 },
+    // { name: 'h3', fontSize: 30, lineHeight: 32 },
+    // { name: 'h3.alt', fontSize: 35, lineHeight: 35 },
+    // { name: 'h4', fontSize: 23, lineHeight: 24 },
+    // { name: 'h5', fontSize: 19, lineHeight: 21 },
+    // { name: 'a', fontSize: 20, lineHeight: 27 },
+    // { name: 'select', fontSize: 20, lineHeight: 20 },
+    // { name: 'label', fontSize: 20, lineHeight: 20 },
+    // { name: 'input', fontSize: 20, lineHeight: 20 },
+    // { name: 'a.nav-link', fontSize: 20, lineHeight: 25 },
+    // { name: 'a.utility-nav', fontSize: 15, lineHeight: 15 },
+    // { name: 'p.eyebrow', fontSize: 22, lineHeight: 22 },
+    // { name: 'button', fontSize: 20, lineHeight: 20 },
+    // { name: '.button', fontSize: 20, lineHeight: 20 },
+    // { name: 'p.XXL', fontSize: 50, lineHeight: 65 },
+    // { name: 'p.XL', fontSize: 26, lineHeight: 32 },
+    // { name: 'p.L', fontSize: 24, lineHeight: 35 },
+    // { name: 'p.M', fontSize: 20, lineHeight: 27 },
+    // { name: 'p.S', fontSize: 18, lineHeight: 27 },
+    // { name: 'p.XS', fontSize: 16, lineHeight: 19 },
+    // { name: 'p.call-out', fontSize: 39, lineHeight: 39 },
+    // { name: 'a.text-link', fontSize: 20, lineHeight: 27 },
+    // { name: 'body .footer label.copyright', fontSize: 14, lineHeight: 27 },
+    // {
+    //   name: 'body .nav-tool-bar .nav-home .menu ul li a',
+    //   fontSize: 17,
+    //   lineHeight: 25
+    // },
+    // {
+    //   name: 'body .footer .footer-right .link-btn a',
+    //   fontSize: 15,
+    //   lineHeight: 15
+    // },
+    // {
+    //   name: 'body .footer .footer-right .link-group .link-list a',
+    //   fontSize: 17,
+    //   lineHeight: 27
+    // }
   ]
 
   if (screenWidth >= 1024) {
